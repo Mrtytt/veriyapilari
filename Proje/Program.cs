65f8d21c83498d13a9b0c13596d4281dc7bca65c
Murat Berk <132847833+Mrtytt@users.noreply.github.com>
2024-05-20 08:13:26 +0300

    Update

--- Proje/Program.cs
+++ Proje/Program.cs
@@ -13,6 +13,19 @@ class Program
 {
     static void Main()
     {
+        // Restoran Yönetim Sistemi
+
+        /*
+            Yiğit Özdemir 032290024
+
+            Barış Işık 032290004
+
+            Murat Berk Yetiştirir 032290008
+
+            Gökay Göncagül 032111004Ç
+
+            Mehmet Halim Baş 032290157
+        */
         Interface.Init();
         Interface.Run();
     }
